68c3c2e999f8b95ba78bbbebbf69bb1a442b37af
Jay Bansal <jay99bansal@gmail.com>
2021-06-07 22:51:43 +0530

    middle element file updated

--- Linked List/MiddleElement.cpp
+++ Linked List/MiddleElement.cpp
@@ -67,8 +67,23 @@ int countNodesLL(struct Node* head) {
 // O(n) algorithm to find and display middle element(s) 
 // in a linked list denoted by head ptr "head"
 void displayMiddleElement(struct Node* head) {
-    // Will do in class today
+    int noElem = countNodesLL(head);
+    struct Node* trav = head;
+    // LL contains 0 or 1 elements
+    if(head == NULL)
+        return;
+    if(head->next == NULL) {
+        cout << "Middle element is : " << head->data << endl;
+        return;
+    }
 
+    for(int i=0; i<noElem/2-1; i++) 
+        trav = trav->next;
+    
+    cout << "Middle element(s) is/are : ";
+    if(noElem%2==0)
+        cout << trav->data << " and ";
+    cout << trav->next->data << endl;
 }
 
 
@@ -76,8 +91,23 @@ void displayMiddleElement(struct Node* head) {
 // in a linked list denoted by head ptr "head"
 // in one pass
 void displayMiddleElement2(struct Node* head) {
-    // Will do in class today
+    struct Node* trav1 = head;
+    struct Node* trav2 = head;
+    // Empty linked list 
+    if(head == NULL)
+        return;
 
+    while(trav2->next!=NULL && trav2->next->next!=NULL) {
+        trav2 = trav2->next->next;
+        trav1 = trav1->next;
+    }
+    cout << "Middle element(s) is/are : ";
+    // odd no of elements
+    if(trav2->next == NULL)
+        cout << trav1->data << endl;
+    // even no of elements
+    else
+        cout << trav1->data << " and " << trav1->next->data << endl;
 }
 
 // O(n) algorithm to delete middle element(s) 
@@ -101,7 +131,24 @@ void displayLL(struct Node* head) {
 
 // Test the functions
 int main() {
-    
+    struct Node* head1 = NULL;
+    head1 = insertAtEndLL(1, head1);
+    head1 = insertAtEndLL(3, head1);
+    head1 = insertAtEndLL(3, head1);
+    head1 = insertAtEndLL(5, head1);
+    // 1 3 3 5
+    cout << "LL1 : ";
+    displayMiddleElement2(head1);
+
+    struct Node* head2 = NULL;
+    head2 = insertAtEndLL(0, head2);
+    head2 = insertAtEndLL(3, head2);
+    head2 = insertAtEndLL(4, head2);
+    head2 = insertAtEndLL(6, head2);
+    head2 = insertAtEndLL(7, head2);
+    // 0 3 4 6 7
+    cout << "LL2 : ";
+    displayMiddleElement2(head2);
     
     return 0;
 }
